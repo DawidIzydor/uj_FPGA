267a3c0bb841f7a3c351122547c465d67c13d8d4
Dawid Izydor <30767613+DawidIzydor@users.noreply.github.com>
2018-03-02 12:08:16 +0100

    End of zajecia 2 marca

--- fpga1/vivado1/vivado1.srcs/sources_1/imports/new/counter1.vhd
+++ fpga1/vivado1/vivado1.srcs/sources_1/imports/new/counter1.vhd
@@ -14,45 +14,38 @@ use IEEE.NUMERIC_STD.ALL;
 library UNISIM;
 use UNISIM.VComponents.all;
 
-
 entity up_down_counter is
     generic (
         NUMBER_OF_BITS : integer := 28
      );
-    port {
+    port (
          CLK : in std_logic;
          RESET : in std_logic;
          LED : out std_logic_vector(3 downto 0);
          UP_IN : in std_logic;
-         DOWN_IN : in std_logic;
-         };
+         DOWN_IN : in std_logic
+         );
 end up_down_counter;
 
 architecture up_down_counter of up_down_counter is
-
+signal counter: unsigned(NUMBER_OF_BITS-1 downto 0);
 begin
 
 COUNTER_PROC: process(CLK)
+begin
     if rising_edge(CLK) then
         if RESET = '1' then
-            counter <= (others ->'0');
+            counter <= (others =>'0');
         elsif UP_IN = '1' and DOWN_IN = '0' then
             counter <= counter +1;
-        elsif UP_IN = '1' and DOWN_IN = '1' then
+        elsif UP_IN = '0' and DOWN_IN = '1' then
             counter <= counter -1;
         else
             counter <= counter;
-        end
-    end
-END process
+        end if;
+    end if;
+END process COUNTER_PROC;
+
+LED <= std_logic_vector(counter(27 downto 24));
+
 end up_down_counter;
-
-
-
-
-architecture Behavioral of counter1 is
-
-begin
-
-
-end Behavioral;
